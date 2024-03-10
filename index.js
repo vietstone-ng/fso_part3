1f76519d7b3ec01f57d181bc6ff0c196a798af93
Viet Nguyen Tran <vietstone.ng@gmail.com>
2024-03-10 22:08:37 +0700

    exercise 3.8

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
   {
